35c3fecf3ec53b61d93ad8e59f7e60eb0abed373
Adam Samec <adam.samec@gmail.com>
2021-05-17 09:12:45 +0200

    Add TODOs and source file download link to Readme.md

--- AccessibleRunner.py
+++ AccessibleRunner.py
@@ -1,5 +1,7 @@
 # TODO
-# * Maybe make the command and args textboxes a single textbox for entering the command also with the arguments.
+# * Maybe make the command and args textboxes a single textbox for entering the command together with the arguments.
+# * Play a notification sound whenever the new output matches a given regular expression. This way the user could be notified about error or successful compilation.
+# * Add a command and working directory history which will be loaded from external file and could be selected ideally using combobox..
 # * Add menubar for macOS. After that, let Cmd + W close only the Window and Cmd + Q close the app.
 
 import os

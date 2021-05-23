b5bbd32bac9eaceeacb9e6c14cff25aa0ea2ded6
Adam <adam.samec@gmail.com>
2021-05-23 16:23:52 +0200

    Add regular expression matching for success and error strings in the output, which can be configured in a new settings dialog

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 # TODO
+# * Add folder chooser dialog for working directory.
 # * Is it possible to send narration message to screen reader?
-# * Play a notification sound whenever the new output matches a given regular expression. This way the user could be notified about error or successful compilation.
-# * Add menubar for macOS. After that, let Cmd + W close only the Window and Cmd + Q close the app.
 
 import os
 import sys
 import wx
+import re
 import psutil
 from subprocess import call, Popen, PIPE, STDOUT
 from threading import Thread
+from playsound import playsound
 
 from config import Config
-from gui import GUI
+from gui import MainFrame
 
 ON_WINDOWS = os.name == 'nt'
 
@@ -23,15 +24,20 @@ class AccessibleRunner:
 
   # Maximum number of items in working directory history.
   DIR_HISTORY_LIMIT = 10
+  
+  # Paths to sounds directory and files
+  SOUNDS_PATH = 'sounds/'
+  SUCCESS_SOUND_PATH = SOUNDS_PATH + 'success.mp3'
+  ERROR_SOUND_PATH = SOUNDS_PATH + 'error.mp3'
 
   # Initializes the object.
   def __init__(self, config):
     self.config = config
     self.process = None
     
-  # Sets the GUI object for this runner.
-  def setGUI(self, gui):
-    self.gui = gui
+  # Sets the UI object for this runner.
+  def setUI(self, ui):
+    self.ui = ui
     
   # Runs the given command in a new process starting in  the  given working directory . The "useShell" parameter indicates if the command should be executed through the shell.
   def runProcess(self, command, dir, useShell):
@@ -55,20 +61,20 @@ class AccessibleRunner:
     try:
       self.process = Popen(command, cwd = dir, shell = useShell, stdout = PIPE, stderr = STDOUT, stdin = PIPE)
     except (NotADirectoryError, FileNotFoundError):         
-      self.gui.setOutput('Error: The working directory \'{}\' does not exist.\n'.format(dir), True)
-      self.gui.setAsNotRunning()
+      self.ui.setOutput('Error: The working directory \'{}\' does not exist.\n'.format(dir), True)
+      self.ui.setAsNotRunning()
     else:
     
       # Start fetching the process output in a new thread
       thread = Thread(target=self.fetchOutput, args = (self.process.stdout, None))
       thread.daemon = True # Thread dies with the program
       thread.start()
-    self.gui.setAsRunning()
+    self.ui.setAsRunning()
     
   # Cleans everything on exit, including saving the changes to the config file.
   def clean(self):
     self.killProcessTree()
-    self.config.save()
+    self.config.saveToFile()
 
   # Adds the given command to the  history.
   def addToCommandHistory(self, command):
@@ -88,7 +94,7 @@ class AccessibleRunner:
     commands = commands[:AccessibleRunner.COMMAND_HISTORY_LIMIT]
     
     # Set the new command choices
-    self.gui.setCommandChoices(commands)
+    self.ui.setCommandChoices(commands)
 
   # Adds the given directory to the  history.
   def addToDirHistory(self, dir):
@@ -109,18 +115,22 @@ class AccessibleRunner:
     dirs = dirs[:AccessibleRunner.DIR_HISTORY_LIMIT]
     
     # Set the new directory choices
-    self.gui.setDirChoices(dirs)
+    self.ui.setDirChoices(dirs)
     
-  # Clears the command output textbox.
+  # Sets the given settings to the config object.
+  def setSettings(self, settings):
+    self.config.settings = settings
+    
+  # Clears the command output.
   def clearOutput(self):
-    self.gui.setOutput('')
+    self.ui.setOutput('')
         
-  # Copies the command output textbox string to the system clipboard.
+  # Copies the command output string to the system clipboard.
   def copyOutput(self):
     if not wx.TheClipboard.IsOpened():
       wx.TheClipboard.Open()
       data = wx.TextDataObject()
-      data.SetText(self.gui.getOutput())
+      data.SetText(self.ui.getOutput())
       wx.TheClipboard.SetData(data)
       wx.TheClipboard.Close()
     
@@ -140,23 +150,41 @@ class AccessibleRunner:
     except:
       pass
     self.process = None
-    self.gui.setAsNotRunning()
+    self.ui.setAsNotRunning()
       
-  # Waits for the process output and continuously writes it to the command output textbox.
+  # Waits for the process output and continuously writes it to the command output. Depending on the current settings, plays success and error sounds if regular expression matches output line.
   def fetchOutput(self, out, arg):
+    settings = self.config.settings
+    
     for line in iter(out.readline, b''):
-      self.gui.setOutput(line.decode(), True)
+      lineString = line.decode()
+      
+      # Play sound if success regex matches
+      if settings['playSuccessSound']:
+        match = re.search(settings['successRegex'], lineString)
+        if match is not None:
+          playsound(AccessibleRunner.SUCCESS_SOUND_PATH)
+        
+      # Play sound if error regex matches
+      if settings['playErrorSound']:
+        match = re.search(settings['errorRegex'], lineString)
+        if match is not None:
+          playsound(AccessibleRunner.ERROR_SOUND_PATH)
+        
+      # Append the line to the UI output
+      self.ui.setOutput(lineString, True)
+
     out.close()
     self.process = None
-    self.gui.setAsNotRunning()
+    self.ui.setAsNotRunning()
     
 # Main function.
 def main():
   app = wx.App()
   config = Config()
   runner = AccessibleRunner(config)
-  gui = GUI(None, runner, config, title='AccessibleRunner')
-  runner.setGUI(gui)
+  mainFrame = MainFrame(runner, config, title='AccessibleRunner')
+  runner.setUI(mainFrame)
   app.MainLoop()
 
 main()

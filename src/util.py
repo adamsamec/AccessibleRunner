9f52cda75edb9fa616b51f91496b5146a747a3d8
Adam <adam.samec@gmail.com>
2021-05-27 20:33:43 +0200

    Add find text feature and other improvements and fixes including Readme.md updates.

---
+++ src/util.py
@@ -0,0 +1,16 @@
+# Utility functions module.
+
+# Returns the line at the given position in the given text.
+def getLine(text, position):
+  # Find the start of the line
+  for startPosition in range(position, 0, -1):
+    if text[startPosition] == '\n':
+      break
+      
+  # Find the end of the line
+  for endPosition in range(position, len(text)):
+    if text[endPosition] == '\r':
+      break
+      
+  return text[startPosition:endPosition]
+  

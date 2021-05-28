69c38263bc979041db9a5f54c4ac38e49e56cb31
Adam <adam.samec@gmail.com>
2021-05-28 17:15:47 +0200

    Update build scripts and add up-to-date dist files

--- build-win32-cx_Freeze.bat
+++ build-win32-cx_Freeze.bat
@@ -1,6 +1,6 @@
 cd src
 C:\Users\asamec\AppData\Local\Programs\Python\Python39-32\python.exe setup-cx_Freeze.py build
 xcopy sounds ..\build\AccessibleRunner-cx_Freeze\sounds\
-xcopy config.orig.json ..\build\AccessibleRunner-cx_Freeze
+xcopy config.default.json ..\build\AccessibleRunner-cx_Freeze
 title build completed
 pause
--- build-win32-pyinstaller.bat
+++ build-win32-pyinstaller.bat
@@ -2,7 +2,7 @@ cd src
 C:\Users\asamec\AppData\Local\Programs\Python\Python39-32\Scripts\pyinstaller.exe -w --distpath ../build --workpath ../temp --specpath ../temp/AccessibleRunner AccessibleRunner.py
 
 xcopy sounds ..\build\AccessibleRunner\sounds\
-xcopy config.orig.json ..\build\AccessibleRunner
+xcopy config.default.json ..\build\AccessibleRunner
 
 echo Deleting the 'temp' directory...
 title Delete the 'temp' directory?

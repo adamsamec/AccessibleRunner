a8c526e464dd6eb5fa3eb75650051be8671b3f52
Adam <adam.samec@gmail.com>
2021-08-24 13:24:22 +0200

    Add Win-32bit and source dist files and update pyinstaller build script

--- build-win32-pyinstaller.bat
+++ build-win32-pyinstaller.bat
@@ -1,12 +1,14 @@
 cd src
-C:\Users\asamec\AppData\Local\Programs\Python\Python39-32\Scripts\pyinstaller.exe -w --distpath ../build --workpath ../temp --specpath ../temp/AccessibleRunner AccessibleRunner.py
+C:\Users\asamec\AppData\Local\Programs\Python\Python39-32\Scripts\pyinstaller.exe -w --additional-hooks-dir . --distpath ../build --workpath ../temp --specpath ../temp/AccessibleRunner AccessibleRunner.py
 
 xcopy sounds ..\build\AccessibleRunner\sounds\
+xcopy md ..\build\AccessibleRunner\md\
 xcopy config.default.json ..\build\AccessibleRunner
 
 echo Deleting the 'temp' directory...
 title Delete the 'temp' directory?
 rmdir /s ..\temp
 
+cd ..
 title Build completed
 pause
